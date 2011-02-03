72cf6fda89dedf61b07ab28071861d046c6f901f
Toni Uebernickel <tuebernickel@gmail.com>
2011-02-03 21:54:35 +0100

    fixed CrontabForm not being configured correctly

--- lib/form/plugin/PluginCrontabForm.class.php
+++ lib/form/plugin/PluginCrontabForm.class.php
@@ -13,6 +13,8 @@ class PluginCrontabForm extends BaseCrontabForm
    */
   public function configure()
   {
+    parent::configure();
+
     $this->setWidget('last_run_at', new sfWidgetFormDateTime(array('default' => date('Y-m-d H:i:s'))));
 
     $this->setValidator('schedule', new CronScheduleValidator(array(), array('invalid' => 'The given schedule is no valid cron schedule entry.')));
