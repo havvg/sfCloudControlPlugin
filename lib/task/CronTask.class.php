ac4fdad01ee057604f1766c3b032398dcbaa7e90
Toni Uebernickel <tuebernickel@gmail.com>
2011-04-15 11:42:36 +0200

    added cloudControl exception handling to CronTask
    The CronTask now responds on UnauthorizedError exception and re-authenticates.
    Any other cloudControl exception will be logged and does not break the CronTask.

--- lib/task/CronTask.class.php
+++ lib/task/CronTask.class.php
@@ -282,17 +282,60 @@ class CronTask extends LoopTask
         /* @var $eachCron Crontab */
         if ($eachCron->isDue())
         {
-          $this->logSection($this->namespace, sprintf('Running command line: %s %s', $eachCron->getCommand(), $eachCron->getParameters()), null, 'INFO');
-          $this->getCloudControl()->addWorker($eachCron->getCommand(), $eachCron->getParameters());
+          try
+          {
+            $this->logSection($this->namespace, sprintf('Running command line: %s %s', $eachCron->getCommand(), $eachCron->getParameters()), null, 'INFO');
+            $this->getCloudControl()->addWorker($eachCron->getCommand(), $eachCron->getParameters());
 
-          $eachCron
-            ->setLastRunAt(new DateTime())
-            ->save($this->getPropelConnection())
-          ;
+            $eachCron
+              ->setLastRunAt(new DateTime())
+              ->save($this->getPropelConnection())
+            ;
+          }
+          /*
+           * The authentication token has expired, so we refresh it and try again.
+           */
+          catch (UnauthorizedError $e)
+          {
+            $this->logCCException($e);
+            $this->getCloudControl()->authenticate();
+
+            /*
+             * This breaks the current checks on the schedule.
+             *
+             * As this is a LoopTask, the execute() method will be called again.
+             * This ensures we are running the current schedule with a valid token again.
+             */
+            return;
+          }
+          /*
+           * The cloudControl exception is caught, so the CronTask may proceed with other workers.
+           * The planned Cron has not been initiated, so the Cron will try again on the next run!
+           */
+          catch (CCException $e)
+          {
+            $this->logCCException($e);
+          }
         }
       }
     }
 
     $this->wait();
+  }
+
+  /**
+   * Log an exception from the cloudControl API.
+   *
+   * @param CCException $e
+   *
+   * @return CronTask $this
+   */
+  protected function logCCException(CCException $e)
+  {
+    // Make sure the error message is displayed full size.
+    $errorMessage = sprintf('Cloudcontrol API returned error: %s (%d)', $e->getMessage(), $e->getCode());
+    $this->logSection($this->namespace, $errorMessage, strlen($errorMessage), 'ERROR');
+
+    return $this;
   }
 }
